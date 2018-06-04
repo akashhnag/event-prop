355ff168eeef6a3eb37f59c335ca34db526d8907
Akash Nag <princ3sscharming28@gmail.com>
2018-06-04 18:14:43 +0530

    commiting event2

--- src/app/event2/event2.component.ts
+++ src/app/event2/event2.component.ts
@@ -6,14 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event2.component.css']
 })
 export class Event2Component implements OnInit {
-  public val
+  public head=["Name","Email","Age"];
+  public details=[{name:"Akash",email:"akash@akash.com",age:25,},
+                  {name:"Deepak",email:"deepak@deepak.com",age:26},
+                  {name:"Sameer",email:"sameer@sameer.com",age:25}]
   constructor() { }
 
   ngOnInit() {
   }
 
-  action(){
-    console.log(this.val);
+  action(a){
+    console.log(a);
     
   }
 
